a8a42e6d9bc0e4ab2165159ff87b0ca1248603fe
Salman Ahmed <salman.ahmed@alpineinfosolutions.com>
2018-10-02 18:41:50 +0530

    App setup done

--- App/App.js
+++ App/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+export default class App extends React.Component{
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDg8UuMvPpHNZ86GqBid_aJJnyfWe4tiXg',
+      authDomain: 'authentication-73.firebaseapp.com',
+      databaseURL: 'https://authentication-73.firebaseio.com',
+      projectId: 'authentication-73',
+      storageBucket: 'authentication-73.appspot.com',
+      messagingSenderId: '715611943987'
+    };
+    firebase.initializeApp(config);
+  }
+  render(){
+    return(
+      <Provider store={createStore(reducers)}>
+       <View style={styles.container}>
+        <Text> Welcome </Text>
+       </View>
+      </Provider>
+
     );
   }
-}
+};
 
 const styles = StyleSheet.create({
   container: {
